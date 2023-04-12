c3352a798da225d573db62fc412e2bbead8029f2
jindal2209 <shubham.jindal@zomato.com>
2023-04-13 03:05:12 +0530

    code refactor

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 const express = require('express');
+const path = require('path')
 const app = express();
-const PORT = 3000
 const attendance_service_router = require('./routes/attendance_service_routes')
-const admin_panel_router = require('./routes/admin_panel_router')
 var cors = require('cors');
+
+const PORT = 3000
 
 app.use(express.json())
 app.use(express.static('build'));
+app.use(cors())
+
+app.use("/attendance_service", attendance_service_router)
 
 app.get('/', (req, res) => {
-    res.json({ 'msg': "200 OK" })
+    res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 })
 
-app.use("/bpit_admin_panel", admin_panel_router)
-app.use("/attendance_service", attendance_service_router)
 
 app.listen(PORT, () => {
     console.log("app listening on port:", PORT)
