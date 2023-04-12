7dcf9a4642d5ca9106244b356718d108bf159725
jindal2209 <shubham.jindal@zomato.com>
2023-04-13 02:52:28 +0530

    added react bundle check

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ const express = require('express');
 const app = express();
 const PORT = 3000
 const attendance_service_router = require('./routes/attendance_service_routes')
+const admin_panel_router = require('./routes/admin_panel_router')
+var cors = require('cors');
 
 app.use(express.json())
+app.use(express.static('build'));
 
 app.get('/', (req, res) => {
     res.json({ 'msg': "200 OK" })
 })
 
-app.use(attendance_service_router)
+app.use("/bpit_admin_panel", admin_panel_router)
+app.use("/attendance_service", attendance_service_router)
 
 app.listen(PORT, () => {
     console.log("app listening on port:", PORT)
